ce9532b2d4f44838e37dff56adcf12976ba555b8
Daniel Leme Junior <daniel.dlj@hotmail.com>
2021-07-25 19:11:28 -0300

    fix: add center_lat and center_lng to state

--- back-end/src/models/state.js
+++ back-end/src/models/state.js
@@ -1,26 +1,38 @@
-const Sequelize = require('sequelize')
-const db = require('../config/database')
+const Sequelize = require("sequelize");
+const db = require("../config/database");
 
-module.exports = db.sequelize.define('state',{
+module.exports = db.sequelize.define(
+  "state",
+  {
     id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    name:{
-        type: Sequelize.STRING,
-        allowNull: false
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
     },
-    sigla:{
-        type: Sequelize.STRING,
-        allowNull: false
-    }
-},{
+    sigla: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    center_lat: {
+      type: Sequelize.DECIMAL(18, 15),
+      allowNull: false,
+    },
+    center_lng: {
+      type: Sequelize.DECIMAL(18, 15),
+      allowNull: false,
+    },
+  },
+  {
     // disable the modification of tablenames; By default, sequelize will automatically
     // transform all passed model names (first parameter of define) into plural.
     // if you don't want that, set the following
     freezeTableName: true,
 
     // define the table's name
-    tableName: 'state'}
-)
+    tableName: "state",
+  }
+);
--- back-end/src/routes/state.js
+++ back-end/src/routes/state.js
@@ -1,33 +1,45 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var State = require('../models/state');
+var State = require("../models/state");
+var City = require("../models/city");
 
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
-  console.log('Time: ', Date.now());
+  console.log("Time: ", Date.now());
   next();
 });
 
+router
+  .route("/")
+  // get all the users (accessed at GET http://localhost:3001/user)
+  .get(function (req, res) {
+    State.findAll()
+      .then((responses) => res.status(200).json(responses))
+      .catch((err) => res.send(err));
+  });
 
-router.route('/')
-    // get all the users (accessed at GET http://localhost:3001/user)
-    .get(function(req, res) {
-        State.findAll()
-            .then(responses => res.status(200).json(responses))
-            .catch(err => res.send(err))
-    });
+router
+  .route("/:stateId")
+  // get the user (accessed at GET http://localhost:3001/user/1)
+  .get(function (req, res) {
+    const id = req.params.stateId;
+    State.findByPk(id)
+      .then((responses) => res.status(200).json(responses))
+      .catch((err) => res.send(err));
+  });
 
-
-
-router.route('/:stateId')
-	// get the user (accessed at GET http://localhost:3001/user/1)
-	.get(function(req, res) {
-        const id = req.params.stateId
-		State.findByPk(id)
-            .then(responses => res.status(200).json(responses))
-            .catch(err => res.send(err))
-	});
-
-
+router
+  .route("/:stateId/citys")
+  // get the user (accessed at GET http://localhost:3001/user/1)
+  .get(function (req, res) {
+    const id = req.params.stateId;
+    City.findAll({
+      where: {
+        stateID: id,
+      },
+    })
+      .then((responses) => res.status(200).json(responses))
+      .catch((err) => res.send(err));
+  });
 
 module.exports = router;
